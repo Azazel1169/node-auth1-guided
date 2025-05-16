ab6a550be07250619bbdb35dc11f78faead3cf44
Josh Rime <joshrime1990@icloud.com>
2025-05-16 01:57:05 -0500

    register route setup

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,9 +1,21 @@
 const express = require("express");
-
+const bcrypt = require("bcryptjs");
 const router = express.Router();
+const Users = require("../users/users-model.js");
 
 router.post("/register", async (req, res, next) => {
-  res.json({ message: "Register Working" });
+  const { username, password } = req.body;
+  const hash = bcrypt.hashSync(password, 12);
+  const newUser = { username, password: hash };
+  // Save the new user to the database
+  try {
+    const result = await Users.add(newUser);
+    res.status(201).json({
+      message: `Nice to have you, ${result.username}`,
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 router.post("/login", async (req, res, next) => {
   res.json({ message: "Login Working" });
