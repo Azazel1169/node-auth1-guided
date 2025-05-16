853c1d04d32f7ec79a48e7a2784881581db375be
Josh Rime <joshrime1990@icloud.com>
2025-05-16 02:40:02 -0500

    login route with working cookie setup

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -3,12 +3,13 @@ const bcrypt = require("bcryptjs");
 const router = express.Router();
 const Users = require("../users/users-model.js");
 
+// Check if user is logged in or registered
 router.post("/register", async (req, res, next) => {
-  const { username, password } = req.body;
-  const hash = bcrypt.hashSync(password, 12);
-  const newUser = { username, password: hash };
-  // Save the new user to the database
   try {
+    const { username, password } = req.body;
+    const hash = bcrypt.hashSync(password, 12);
+    const newUser = { username, password: hash };
+    // Save the new user to the database
     const result = await Users.add(newUser);
     res.status(201).json({
       message: `Nice to have you, ${result.username}`,
@@ -17,8 +18,23 @@ router.post("/register", async (req, res, next) => {
     next(err);
   }
 });
+
+// Login route
 router.post("/login", async (req, res, next) => {
-  res.json({ message: "Login Working" });
+  try {
+    const { username, password } = req.body;
+    const [user] = await Users.findBy({ username });
+    if (user && bcrypt.compareSync(password, user.password)) {
+      req.session.user = user;
+      res.json({
+        message: `Welcome back, ${user.username}`,
+      });
+    } else {
+      next({ status: 401, message: "Invalid credentials" });
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 router.get("/logout", async (req, res, next) => {
   res.json({ message: "Logout Working" });
--- api/server.js
+++ api/server.js
@@ -1,13 +1,26 @@
 const path = require("path");
 const express = require("express");
-
+const session = require("express-session");
 const usersRouter = require("./users/users-router.js");
 const authRouter = require("./auth/auth-router.js");
 const server = express();
 
 server.use(express.static(path.join(__dirname, "../client")));
 server.use(express.json());
-
+server.use(
+  session({
+    name: "monkey",
+    secret: "keep it secret",
+    cookie: {
+      maxAge: 1000 * 60 * 60, // 1 hour
+      secure: false, // set to true if using https
+      httpOnly: false, // set to true to prevent client-side JS from accessing the cookie
+    },
+    rolling: true, // reset maxAge on every response
+    resave: false,
+    saveUninitialized: false, // don't create session until something stored
+  })
+);
 server.use("/api/users", usersRouter);
 server.use("/api/auth", authRouter);
 
